75d821e722c14a79d84c2993b0322fdec2066f25
Francesca Santoriello <f.santoriello@hotmail.com>
2024-03-03 09:38:40 +0100

    damping, handle resizing & pixel ratio

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 const canvas = document.querySelector('canvas#webgl');
 
 // Scene
+// https://threejs.org/docs/#api/en/scenes/Scene
 const scene = new THREE.Scene();
 
-// Object
+// Object(s)
+// https://threejs.org/docs/#api/en/objects/Mesh
 const mesh = new THREE.Mesh(
     new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
@@ -20,6 +22,7 @@ const sizes = {
     height: window.innerHeight 
 };
 
+// handle resize
 window.addEventListener('resize', (e) => {
     // update sizes
     sizes.width  = window.innerWidth;
@@ -33,7 +36,14 @@ window.addEventListener('resize', (e) => {
     renderer.setSize(sizes.width, sizes.height);
 });
 
+// handle fullscreen
+window.addEventListener('dblclick', () => {
+    //                                    leave fullscreen : enter fullscreen
+    document.fullscreenElement ? document.exitFullscreen() : canvas.requestFullscreen();
+});
+
 // Camera
+// https://threejs.org/docs/#api/en/cameras/PerspectiveCamera
 // field of view (vertical vision angle), aspect ratio,                  _near, _far 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 3;
@@ -42,13 +52,15 @@ scene.add(camera);
 camera.lookAt(mesh.position);
 
 // Controls
+// https://threejs.org/docs/#examples/en/controls/OrbitControls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 // Renderer
+// https://threejs.org/docs/#api/en/renderers/WebGLRenderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // Animations
 const animLoop = () => {
