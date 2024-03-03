83d862e3f252e776d06e1f7bf79778f022f22b5e
Francesca Santoriello <f.santoriello@hotmail.com>
2024-03-03 09:21:41 +0100

    damping, handle resizing & pixel ratio

--- src/main.js
+++ src/main.js
@@ -1,15 +1,6 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// listen to cursor state
-const cursor = { x: 0, y: 0 };
-window.addEventListener('mousemove', (e) => {
-    // stabilise to -0.5 .. 0.5
-    cursor.x = (e.clientX / sizes.width - 0.5);
-    cursor.y = - (e.clientY / sizes.height - 0.5);
-    // correct position.y VS clientY inversion
-});
-
 // Canvas
 const canvas = document.querySelector('canvas#webgl');
 
@@ -23,9 +14,26 @@ const mesh = new THREE.Mesh(
 );
 scene.add(mesh);
 
-// Camera
-const sizes = { width: 800, height: 600 };
+// Window
+const sizes = { 
+    width: window.innerWidth, 
+    height: window.innerHeight 
+};
 
+window.addEventListener('resize', (e) => {
+    // update sizes
+    sizes.width  = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // update camera
+    camera.aspect = (sizes.width / sizes.height);
+    camera.updateProjectionMatrix();
+
+    // update renderer
+    renderer.setSize(sizes.width, sizes.height);
+});
+
+// Camera
 // field of view (vertical vision angle), aspect ratio,                  _near, _far 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 3;
@@ -33,15 +41,20 @@ camera.position.z = 3;
 scene.add(camera);
 camera.lookAt(mesh.position);
 
-// &controls
+// Controls
 const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(window.devicePixelRatio);
 
 // Animations
 const animLoop = () => {
+    // to enable damping
+    controls.update();
+
     renderer.render(scene, camera);
     window.requestAnimationFrame(animLoop);
 }
