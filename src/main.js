9f716f96736cd03232ae787db59e986610d2ea44
Francesca Santoriello <f.santoriello@hotmail.com>
2024-03-03 12:14:12 +0100

    lil-gui debugger ++

--- src/main.js
+++ src/main.js
@@ -1,47 +1,71 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from 'gsap'
 import GUI from 'lil-gui';
 
 // Debugger
-const gui      = new GUI();
-const guiProps = { 
-    color: '#f297d9' 
+const gui      = new GUI({ width: 300 });
+const guiObject = { 
+    color: '#a778d8',
+    subdivision: 2
 };
+
+// show/hide on h keystroke
+window.addEventListener('keydown', (e) => {
+    if(e.key === 'h')
+        gui.show(gui._hidden)
+});
 
 // Canvas
 const canvas = document.querySelector('canvas#webgl');
 
 // Scene
-// https://threejs.org/docs/#api/en/scenes/Scene
 const scene = new THREE.Scene();
 
 // Object(s)
-// https://threejs.org/docs/#api/en/geometries/BoxGeometry
-// x, y, z, _widthSegments, _heightSegments, _depthSegments
-const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: guiProps.color });
+const geometry = new THREE.BoxGeometry(
+    1, 1, 1,
+    guiObject.subdivision, guiObject.subdivision, guiObject.subdivision
+);
+const material = new THREE.MeshBasicMaterial({ color: guiObject.color });
 const mesh     = new THREE.Mesh(geometry, material);
-// https://threejs.org/docs/#api/en/objects/Mesh
-
 scene.add(mesh);
 
-// &add to debugger
-//   object, prop to tweak
+// &add to debugger, object n prop
 gui
     .add(mesh.position, 'y')
     .min(-3).max(3).step(0.01)
     .name('elevation');
-//            for controls
-
-gui.add(mesh, 'visible');
-gui.add(material, 'wireframe');
 
 gui
-    .addColor(guiProps, 'color')
-    .onChange((value) => {
-        // stabilize threejs color codes
-        material.color.set(guiProps.color);
+    .add(guiObject, 'subdivision')
+    .min(1).max(10).step(1)
+    .onFinishChange(() => {
+        mesh.geometry.dispose();
+        mesh.geometry = new THREE.BoxGeometry(
+            1, 1, 1,
+            guiObject.subdivision, guiObject.subdivision, guiObject.subdivision
+        );
     });
+
+gui
+    .add(mesh, 'visible');
+
+gui
+    .add(material, 'wireframe');
+
+gui
+    .addColor(guiObject, 'color')
+    .onChange(() => {
+        material.color.set(guiObject.color);
+    });
+
+guiObject.spin = () => {
+    //                           full circle rotation
+    gsap.to(mesh.rotation, { y: (mesh.rotation.y + Math.PI * 2), duration: 3 });
+}
+gui
+    .add(guiObject, 'spin');
 
 // Window
 const sizes = { 
@@ -70,7 +94,6 @@ window.addEventListener('dblclick', () => {
 });
 
 // Camera
-// https://threejs.org/docs/#api/en/cameras/PerspectiveCamera
 // field of view (vertical vision angle), aspect ratio,                  _near, _far 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
 camera.position.z = 3;
@@ -79,12 +102,10 @@ scene.add(camera);
 camera.lookAt(mesh.position);
 
 // Controls
-// https://threejs.org/docs/#examples/en/controls/OrbitControls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 // Renderer
-// https://threejs.org/docs/#api/en/renderers/WebGLRenderer
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
